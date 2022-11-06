ed7d9d179ac49b4ea3db12833dc8f8a84ceb7a2a
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-11-06 22:21:41 +0600

    upading

--- src/games/Games.js
+++ src/games/Games.js
@@ -1,11 +1,40 @@
 import React from 'react';
 import '../games/games.css';
 const Games = () => {
+    const X_CLASS = 'x'
+    const CIRCLE_CLASS = 'circle'
+    const cellElelments = document.querySelectorAll('[data-cell]')
+    const board = document.getElementById('board')
+    let circleTurn
+    cellElelments.forEach(cell => {
+        cell.addEventListener('click', handleClick, { once: true })
+    })
+
+    function handleClick(e) {
+        const cell = e.target
+        const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+        placeMark(cell, currentClass)
+
+        swapTurns()
+        setBoardHoverClass()
+    }
+
+    function placeMark(cell, currentClass) {
+        cell.classList.add(currentClass);
+    }
+
+    function swapTurns() {
+        circleTurn = !circleTurn
+    }
+
+    function setBoardHoverClass() {
+
+    }
     return (
         <div>
-            <div className='board x' id='board'>
-                <div className='cell x' data-cell></div>
-                <div className='cell circle' data-cell></div>
+            <div className='board' id='board'>
+                <div className='cell ' data-cell></div>
+                <div className='cell ' data-cell></div>
                 <div className='cell' data-cell></div>
                 <div className='cell' data-cell></div>
                 <div className='cell' data-cell></div>
