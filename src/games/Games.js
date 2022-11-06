cec59d6e6320f0451b33dab8ad36ebe638b75fc7
Md Nabiul Bashar <nabiultasbed@gmail.com>
2022-11-06 22:40:11 +0600

    game program ad

--- src/games/Games.js
+++ src/games/Games.js
@@ -3,18 +3,35 @@ import '../games/games.css';
 const Games = () => {
     const X_CLASS = 'x'
     const CIRCLE_CLASS = 'circle'
+    const WINNING_COMBINATIONS = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ]
     const cellElelments = document.querySelectorAll('[data-cell]')
     const board = document.getElementById('board')
     let circleTurn
-    cellElelments.forEach(cell => {
-        cell.addEventListener('click', handleClick, { once: true })
-    })
+    startGame()
+    function startGame(params) {
+        circleTurn = false
+        cellElelments.forEach(cell => {
+            cell.addEventListener('click', handleClick, { once: true })
+        })
+        setBoardHoverClass()
+    }
 
     function handleClick(e) {
         const cell = e.target
         const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
         placeMark(cell, currentClass)
+        if (checkWin(currentClass)) {
 
+        }
         swapTurns()
         setBoardHoverClass()
     }
@@ -35,6 +52,14 @@ const Games = () => {
         } else {
             board.classList.add(X_CLASS)
         }
+    }
+
+    function checkWin(currentClass) {
+        return WINNING_COMBINATIONS.some(combination => {
+            return combination.every(index => {
+                return cellElelments[index].classList.contains(currentClass)
+            })
+        })
     }
     return (
         <div>
